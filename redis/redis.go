32c3c77b60b27098c5893ecf76a02ff3637fd916
Hirokazu Miyaji <hirokazu.miyaji@gmail.com>
2015-09-09 16:46:45 +0900

    add redis worker

--- redis/redis.go
+++ redis/redis.go
@@ -1,35 +1,80 @@
 package redis
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
 	"github.com/garyburd/redigo/redis"
 )
 
-type RedisConfig struct {
-	Host     string
-	Port     int
-	DB       int
-	Password string
-	Socker   string
-}
+type HandlerFunc func([]byte) error
 
 type Worker struct {
-	conn      *redis.Conn
-	queueName string
+	wg         *sync.WaitGroup
+	connection redis.Conn
+	queueName  string
+	handler    HandlerFunc
+	signal     chan os.Signal
 }
 
-func NewWorker(config *RedisConfig, queueName string) *Worker {
-	connection := nil
+func NewWorker(wg *sync.WaitGroup, connection redis.Conn, queueName string, handler HandlerFunc) *Worker {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGKILL)
 	return &Worker{
-		conn:      connection,
-		queueName: queueName,
+		wg:         wg,
+		connection: connection,
+		queueName:  queueName,
+		handler:    handler,
+		signal:     sig,
 	}
 }
 
-func (w *Worker) Start() error {
+func (w *Worker) Start() {
+	w.wg.Add(1)
+	go w.backgroundLoop()
 }
 
-func (w *Worker) Shutdown() error {
-	if err := w.conn.Close(); err != nil {
-		return err
+func (w *Worker) Shutdown() {
+	defer w.wg.Done()
+	w.connection.Close()
+}
+
+func (w *Worker) backgroundLoop() {
+	errorCount := 0
+loop:
+	for {
+		select {
+		case _ = <-w.signal:
+			signal.Stop(w.signal)
+			break loop
+		default:
+			var data interface{}
+			data, err := w.connection.Do("BRPOP", w.queueName, 1)
+			if err != nil {
+				errorCount += 1
+				continue
+			}
+
+			errorCount = 0
+			if data == nil {
+				continue
+			}
+
+			values, err := redis.Values(data, err)
+			if err != nil {
+				errorCount += 1
+				continue
+			}
+
+			value := values[1].([]byte)
+			if err := w.handler(values[1].([]byte)); err != nil {
+				w.connection.Do("LPUSH", fmt.Sprintf("%s:FAIL", w.queueName), value)
+			}
+		}
 	}
+
+	w.Shutdown()
 }
